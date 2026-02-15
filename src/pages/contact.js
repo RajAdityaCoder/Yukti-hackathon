cfa7ca583ee00aea5c68fa23a25b0ebbf6e65de3
Aditya Raj Chaudhary <adi@Adityas-MacBook-Air.local>
2026-02-15 13:31:28 +0530

    final commit result announcement 6

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -140,7 +140,7 @@ const Contact = () => {
         </p>
       </div>
 
-      <div className="max-w-6xl mx-auto mt-10 grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-3 gap-6 ">
+      <div className="max-w-6xl mx-auto mt-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 ">
         {contacts.map((person, index) => (
           <ContactCard
             key={index}
